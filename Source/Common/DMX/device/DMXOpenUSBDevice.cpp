6e6107219e5f4e549742a821cbf466d583c53c60
Ben Kuper <bkuperberg@gmail.com>
2020-11-01 17:50:13 +0100

    handle IOException fix crash

--- Source/Common/DMX/device/DMXOpenUSBDevice.cpp
+++ Source/Common/DMX/device/DMXOpenUSBDevice.cpp
@@ -1,9 +1,9 @@
 /*
   ==============================================================================
 
-    DMXOpenUSBDevice.cpp
-    Created: 7 Apr 2017 11:22:57am
-    Author:  Ben
+	DMXOpenUSBDevice.cpp
+	Created: 7 Apr 2017 11:22:57am
+	Author:  Ben
 
   ==============================================================================
 */
@@ -11,8 +11,8 @@
 #include "DMXOpenUSBDevice.h"
 
 #if JUCE_LINUX
-	#include <sys/ioctl.h>
-	#include <asm/termbits.h>
+#include <sys/ioctl.h>
+#include <asm/termbits.h>
 #endif
 
 DMXOpenUSBDevice::DMXOpenUSBDevice() :
@@ -26,48 +26,56 @@ DMXOpenUSBDevice::~DMXOpenUSBDevice()
 
 void DMXOpenUSBDevice::setPortConfig()
 {
-	dmxPort->port->setBaudrate(250000);
-	dmxPort->port->setBytesize(serial::eightbits);
-	dmxPort->port->setStopbits(serial::stopbits_two);
-	dmxPort->port->setParity(serial::parity_none);
-	dmxPort->port->setFlowcontrol(serial::flowcontrol_none);
-	dmxPort->port->setRTS(false);
-	dmxPort->port->setDTR(false);
-	dmxPort->port->flush();
+	try
+	{
+
+		dmxPort->port->setBaudrate(250000);
+		dmxPort->port->setBytesize(serial::eightbits);
+		dmxPort->port->setStopbits(serial::stopbits_two);
+		dmxPort->port->setParity(serial::parity_none);
+		dmxPort->port->setFlowcontrol(serial::flowcontrol_none);
+		dmxPort->port->setRTS(false);
+		dmxPort->port->setDTR(false);
+		dmxPort->port->flush();
 
 #if JUCE_LINUX
-	#if defined(TCGETS2)
-	    int fd = dmxPort->port->getHandle();
-	    static const int rate = 250000;
+#if defined(TCGETS2)
+		int fd = dmxPort->port->getHandle();
+		static const int rate = 250000;
 
-	    struct termios2 tio;  // linux-specific terminal stuff
+		struct termios2 tio;  // linux-specific terminal stuff
 
-	    if (ioctl(fd, TCGETS2, &tio) < 0)
-	    {
-	        DBG("OpenDMX Error in getting termios2 data");
-	        return;
-	    }
+		if (ioctl(fd, TCGETS2, &tio) < 0)
+		{
+			DBG("OpenDMX Error in getting termios2 data");
+			return;
+		}
 
-	    tio.c_cflag &= ~CBAUD;
-	    tio.c_cflag |= BOTHER;
-	    tio.c_ispeed = 250000;
-	    tio.c_ospeed = 250000;  // set custom speed directly
-	    if (ioctl(fd, TCSETS2, &tio) < 0)
-	    {
-	        DBG("[UARTWidget] Error in setting termios2 data");
-	        return;
-	    }
-	#endif
+		tio.c_cflag &= ~CBAUD;
+		tio.c_cflag |= BOTHER;
+		tio.c_ispeed = 250000;
+		tio.c_ospeed = 250000;  // set custom speed directly
+		if (ioctl(fd, TCSETS2, &tio) < 0)
+		{
+			DBG("[UARTWidget] Error in setting termios2 data");
+			return;
+		}
 #endif
+#endif
+	}
+	catch (serial::IOException e)
+	{
+		LOGERROR("Error opening OpenDMX : " << e.what());
+	}
 
 }
 
 void DMXOpenUSBDevice::sendDMXValuesSerialInternal()
 {
-	dmxPort->port->setBreak(true);
-	dmxPort->port->setBreak(false);
 	try
 	{
+		dmxPort->port->setBreak(true);
+		dmxPort->port->setBreak(false);
 		dmxPort->port->write(startCode, 1); //start code
 		dmxPort->port->write(dmxDataOut, 512);
 	}
