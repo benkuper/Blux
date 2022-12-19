5d0cf454f309790ad58cae29342288f594f8d44b
Ben Kuper <bkuperberg@gmail.com>
2022-12-19 17:52:59 +0100

    Invert filter for ID fix

--- Source/Effect/Filter/filters/id/IDFilter.cpp
+++ Source/Effect/Filter/filters/id/IDFilter.cpp
@@ -1,25 +1,25 @@
 /*
   ==============================================================================
 
-    IDFilter.cpp
-    Created: 3 Oct 2020 10:31:42am
-    Author:  bkupe
+	IDFilter.cpp
+	Created: 3 Oct 2020 10:31:42am
+	Author:  bkupe
 
   ==============================================================================
 */
 
 IDFilter::IDFilter() :
-    Filter(getTypeString()),
-    ids("IDs")
+	Filter(getTypeString()),
+	ids("IDs")
 {
-    //mode = addEnumParameter("Mode", "Filtering mode");
-    //mode->addOption("Include", true)->addOption("Exclude", false);
+	//mode = addEnumParameter("Mode", "Filtering mode");
+	//mode->addOption("Include", true)->addOption("Exclude", false);
 
-    saveAndLoadRecursiveData = false;
+	saveAndLoadRecursiveData = false;
 
-    ids.userCanAddControllables = true;
-    ids.userAddControllablesFilters.add(IntParameter::getTypeStringStatic());
-    addChildControllableContainer(&ids);
+	ids.userCanAddControllables = true;
+	ids.userAddControllablesFilters.add(IntParameter::getTypeStringStatic());
+	addChildControllableContainer(&ids);
 }
 
 IDFilter::~IDFilter()
@@ -28,48 +28,55 @@ IDFilter::~IDFilter()
 
 bool IDFilter::isAffectingObject(Object* o)
 {
-    int oid = o->globalID->intValue();
-    for (int i = 0; i < ids.controllables.size(); i++) if (oid == ((IntParameter*)ids.controllables[i])->intValue()) return true;
-    return false;
+	int oid = o->globalID->intValue();
+	for (int i = 0; i < ids.controllables.size(); i++)
+	{
+		if (oid == ((IntParameter*)ids.controllables[i])->intValue())
+		{
+			return !invert->boolValue();
+		}
+	}
+
+	return invert->boolValue();
 }
 
 FilterResult IDFilter::getFilteredResultForComponentInternal(Object* o, ObjectComponent* c)
 {
-    int oid = o->globalID->intValue();
-    for (int i = 0; i < ids.controllables.size(); i++) if (oid == ((IntParameter*)ids.controllables[i])->intValue()) return FilterResult({ i, 1 });
-    return FilterResult();
+	int oid = o->globalID->intValue();
+	for (int i = 0; i < ids.controllables.size(); i++) if (oid == ((IntParameter*)ids.controllables[i])->intValue()) return FilterResult({ i, 1 });
+	return FilterResult();
 }
 
 void IDFilter::controllableAdded(Controllable* c)
 {
-    if (c->parentContainer == &ids && !isCurrentlyLoadingData)
-    {
-        c->isRemovableByUser = true;
-        c->setNiceName("ID");
-        ((IntParameter*)c)->setRange(0, INT32_MAX);
-    }
+	if (c->parentContainer == &ids && !isCurrentlyLoadingData)
+	{
+		c->isRemovableByUser = true;
+		c->setNiceName("ID");
+		((IntParameter*)c)->setRange(0, INT32_MAX);
+	}
 }
 
 var IDFilter::getJSONData()
 {
-    var data = Filter::getJSONData();
-    var idData;
-    for (auto& c : ids.controllables)
-    {
-        idData.append(((Parameter*)c)->intValue());
-    }
-    data.getDynamicObject()->setProperty("ids", idData);
-    return data;
+	var data = Filter::getJSONData();
+	var idData;
+	for (auto& c : ids.controllables)
+	{
+		idData.append(((Parameter*)c)->intValue());
+	}
+	data.getDynamicObject()->setProperty("ids", idData);
+	return data;
 }
 
 void IDFilter::loadJSONDataInternal(var data)
 {
-    Filter::loadJSONDataInternal(data);
-    var idData = data.getProperty("ids", var());
-    for (int i = 0; i < idData.size(); i++)
-    {
-        IntParameter* ip = ids.addIntParameter("ID", "", 0, 0);
-        ip->isRemovableByUser = true;
-        ip->setValue((int)idData[i]);
-    }
+	Filter::loadJSONDataInternal(data);
+	var idData = data.getProperty("ids", var());
+	for (int i = 0; i < idData.size(); i++)
+	{
+		IntParameter* ip = ids.addIntParameter("ID", "", 0, 0);
+		ip->isRemovableByUser = true;
+		ip->setValue((int)idData[i]);
+	}
 }
