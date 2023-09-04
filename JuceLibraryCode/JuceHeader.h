2d893ed4e6895516309b4eac6a970a1ee0028963
Ben Kuper <bkuperberg@gmail.com>
2023-09-04 12:02:52 +0200

    1.2.0b7

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -50,7 +50,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Blux";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.2.0b6";
+    const char* const  versionString  = "1.2.0b7";
     const int          versionNumber  = 0x10200;
 }
 #endif
