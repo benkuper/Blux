a58777e898fce633d8a6f939f776b1f65f0ed102
Ben Kuper <bkuperberg@gmail.com>
2023-11-23 14:44:28 +0100

    better rgb transparent viz

--- Source/Color/ColorSource/ui/ColorViz.cpp
+++ Source/Color/ColorSource/ui/ColorViz.cpp
@@ -51,7 +51,7 @@ void ColorViz::paint(Graphics& g)
         g.setColour(c);
         g.fillEllipse(pr);
 
-        g.setColour(c.withAlpha(.5f));
+        g.setColour(c.brighter(.6f).withAlpha(.7f));
         g.drawEllipse(pr, 1);
     }
 }
